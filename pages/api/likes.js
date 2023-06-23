5785e3dc455fdf7f744876c01b6bda615bebbf5a
Matthias <113374253+Asbestcode@users.noreply.github.com>
2023-06-23 15:13:10 +0200

    created like model

--- pages/api/likes.js
+++ pages/api/likes.js
@@ -1,8 +1,11 @@
 import { initMongoose } from "@/lib/mongoose";
+import { getServerSession } from "next-auth";
 
 export default async function handle(req, res) {
     await initMongoose();
-
-    const {id} = req.body;
+    const session = await getServerSession(req, res, authOptions);
+    
+    const postId = req.body.id;
+    const userId = session.user.id
     res.json({id})
 }
