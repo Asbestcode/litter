ebe7ca7e2c69c4a328118ed4af91cd7da87ec80d
Matthias <113374253+Asbestcode@users.noreply.github.com>
2023-06-24 13:10:40 +0200

    added logOut function

--- hooks/useUserInfo.js
+++ hooks/useUserInfo.js
@@ -3,23 +3,27 @@ import {useSession} from "next-auth/react";
 
 export default function useUserInfo() {
 
-  const {data: session, status: sessionStatus} = useSession();
-  const [userInfo, setUserInfo] = useState();
+  const {data:session, status:sessionStatus} = useSession();
+  const [userInfo, setUserInfo] = useState(null);
   const [status, setStatus] = useState('loading');
 
   function getUserInfo() {
     if (sessionStatus === 'loading') {
       return;
     }
-    if (sessionStatus === 'unauthenticated') {
-      setStatus('unauthenticated');
+    if (!session?.user?.id) {
+      setStatus('unauthenticated')
       return;
     }
+    // if (sessionStatus === 'unauthenticated') {
+    //   setStatus('unauthenticated');
+    //   return;
+    // }
     fetch('/api/users?id='+session.user.id)
       .then(response => {
         response.json().then(json => {
           setUserInfo(json.user);
-          setStatus('done');
+          setStatus('authenticated');
         })
       })
   }
