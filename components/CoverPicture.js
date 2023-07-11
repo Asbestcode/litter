b1ab4f19b2e3e509a188d6c7c46b6954af43370d
Matthias <113374253+Asbestcode@users.noreply.github.com>
2023-07-11 19:42:11 +0200

    fix

--- components/CoverPicture.js
+++ components/CoverPicture.js
@@ -1,10 +1,11 @@
 import { FileDrop } from "react-file-drop"
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { PulseLoader } from "react-spinners";
 
 export default function CoverPicture({src, onChange, editable=false}) {
     const [isFileNearby, setIsFileNearby] = useState(false);
     const [isUploading, setIsUploading] = useState(false);
+    const fileInputRef = useRef(null);
     let extraClasses = '';
     if (isFileNearby) extraClasses += ' bg-green-500 opacity-60';
     if (!editable) extraClasses = '';
@@ -28,6 +29,29 @@ export default function CoverPicture({src, onChange, editable=false}) {
         })
     }
 
+    const onFileInputChange = (e) => {
+        if (!editable) {
+            return;
+        }
+        e.preventDefault();
+        setIsUploading(true);
+        const { files } = e.target;
+        const data = new FormData();
+        data.append('post', files[0]);
+        fetch('/api/upload', {
+          method: 'POST',
+          body: data,
+        }).then(async response => {
+          const json = await response.json();
+          onChange(json.src);
+          setIsUploading(false);
+        })
+      }
+    
+      const onTargetClick = () => {
+        fileInputRef.current.click()
+      }
+
     return (
         <div className="overflow-hidden">
             <FileDrop
@@ -37,6 +61,12 @@ export default function CoverPicture({src, onChange, editable=false}) {
                 onFrameDrop={() => setIsFileNearby(false)}
             >
                 <div className="relative bg-litterBorder text-white">
+                    <input 
+                        onChange={onFileInputChange}
+                        ref={fileInputRef}
+                        type="file"
+                        className="hidden" 
+                    />
                     <div className={'absolute inset-0 z-10 '+extraClasses}></div>   
                     {isUploading && (    
                         <div className="absolute inset-0 flex items-center justify-center bg-green-500 opacity-60"> 
@@ -45,7 +75,7 @@ export default function CoverPicture({src, onChange, editable=false}) {
                     )}
                     <div className="flex aspect-w-7 aspect-h-3 w-full overflow-hidden">
                         {src && (<img src={src} className="object-cover object-top" alt=""/>)}
-                        {!src && (<div className="w-full flex justify-center items-center">{editable ? 'drop image!' : 'no image'}</div>)}
+                        {!src && (<div className="w-full flex justify-center items-center">{editable ? 'choose image!' : 'no image'}</div>)}
                     </div>
                 </div>
             </FileDrop>
